8010c69f79758ae1ba38749de1dfb0f45c29dbb8
jackie2806 <jaishackeline@gmail.com>
2022-09-21 13:31:01 -0500

    Commit de prueba

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = () => {
   // ...
 };
+console.log('hola');
+// en index.js irá la función mdLink
+// Instalar jest slint npm 
+// crear un archivo para las funciones chiquitas
+// Test: usar espías 
+const prueba = './README.md';
+console.log(prueba);
---
+++ tools/tooling.md
@@ -0,0 +1,1 @@
+// buscar en internet los formatos mdlinks
