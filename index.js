39ca70ff81eb32ce4700325025ad13e06cdd1e24
jackie2806 <jaishackeline@gmail.com>
2022-09-22 11:33:49 -0500

    instalación del script test y start

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = () => {
 console.log('hola');
 // en index.js irá la función mdLink
 // Instalar jest slint npm 
-// crear un archivo para las funciones chiquitas
 // Test: usar espías 
 const prueba = './README.md';
 console.log(prueba);
+//path fs import 
