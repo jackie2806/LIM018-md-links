0aec5884a9d5fe8ac8393ee08e2c9cf4429e48d6
jackie2806 <jaishackeline@gmail.com>
2022-10-04 06:52:16 -0500

    función que verifica el tipo de ruta implementada

--- src/index.js
+++ src/index.js
@@ -1,42 +1,38 @@
-module.exports = () => {
-};
-
-//Haciendo pruebas con Node
-/* let miNombre = process.env.NOMBRE || 'Sin nombre';
-console.log('Hola, '+ miNombre); */
 
 const prueba = './README.md';
-console.log(prueba);
-// Comprensión de la asincronía
-/* function soyAsincrona(miCallback){
-  setTimeout(() => {
-    console.log('Estoy siendo asíncrona');
-    miCallback();
-  }, 1000);
+// console.log(prueba);
 
-}
-console.log('Iniciando proceso...');
-soyAsincrona(function(){
-  console.log('Terminando proceso');
-}) */
 // en index.js irá la función mdLink
-// Instalar jest slint npm 
 // Test: usar espías 
 //path fs import 
 // Métodos de Node (path / fs)
-const { existsSync } = require('fs'); // desestructuración de objetos
+// const { existsSync } = require('fs'); // desestructuración de objetos
+const fs = require('fs');
 const path = require('path');
-
 
 // 1. Función que verifica si la ruta existe
 const verifyRoute = (route) => {
-  console.log(existsSync(route));
-  return existsSync(route)
+  console.log('La ruta existe?', fs.existsSync(route));
+  return fs.existsSync(route)
 }
 verifyRoute('./tools/reading.word')
 
-// 2. Función que verifica el tipo de ruta
+// 2. Función que verifica el tipo de ruta. Si no es absoluta, la covierte a absoluta
+const typeRoute = (route) => {
+  console.log('Es una ruta absoluta?', path.isAbsolute(route))
+  if(path.isAbsolute(route)){
+    return path.isAbsolute(route)
+  } else {
+    console.log('Conversión de la ruta a absoluta: ', path.resolve(route))
+    return path.resolve(route);
+  }
+}
+typeRoute('./tools/reading.word');
 
 // 3. Función que lee la extensión del archivo
 
-// 4. Implementar los test
+
+
+
+module.exports = () => {
+};
--- src/small-functions.js
+++ src/small-functions.js
@@ -1,4 +1,5 @@
-const myObj = {
+// Explicación de desestructuración de objetos
+/* const myObj = {
     nameX: 'rita',
     lastName: 'acevedo'
 }
@@ -11,5 +12,43 @@ const { nameX, lastName } = myObj
 console.log(nameX, lastName)
 
 module.exports = {
-    existsSync: function()
+    existsSync: function () {
+
+    },
+} */
+
+//Haciendo pruebas con Node
+/* let miNombre = process.env.NOMBRE || 'Sin nombre';
+console.log('Hola, '+ miNombre); */
+
+// Comprensión de la asincronía
+/* function soyAsincrona(miCallback){
+  setTimeout(() => {
+    console.log('Estoy siendo asíncrona');
+    miCallback();
+  }, 1000);
+
 }
+console.log('Iniciando proceso...');
+soyAsincrona(function(){
+  console.log('Terminando proceso');
+}) */
+// Algunas cositas de NODE muy útiles 
+/*
+console.log(__dirname);
+console.log(__filename); 
+console.table();
+const os = require('os');
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.cpus().length);
+const SIZE = 1024;
+function kb(bytes) { return bytes / SIZE}
+function mb(bytes) { return kb(bytes) / SIZE}
+function gb(bytes) { return mb(bytes) / SIZE}
+console.log(os.freemem());
+console.log(kb(os.freemem()));
+console.log(mb(os.freemem()));
+console.log(gb(os.freemem()));
+console.log(gb(os.totalmem())); 
+*/
