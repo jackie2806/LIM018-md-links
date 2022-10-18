3206ffb770d6a69d0c2a73de82c25dd1e800ffd4
jackie2806 <jaishackeline@gmail.com>
2022-10-18 10:54:46 -0500

    función mdLinks creada

--- src/index.js
+++ src/index.js
@@ -3,39 +3,45 @@ const fn = require('./src/small-functions.js');
 
 const mdLinks = (route, options) => {
   const promise =  new Promise((resolve, reject) => {
+      // Verifica si la ruta existe
     if(!fn.verifyRoute(route)){
-      reject(new Error('La ruta no existe'));
-    } 
-    // Verificar el tipo de ruta
-    const absoluteRoute = fn.verifyAbsoluteRoute(route);
-    const arrLinks = [];
-    if(absoluteRoute){
-      absoluteRoute;
+      reject(new Error('La ruta no existe, ingrese una ruta válida'));
     } else {
-      
-    }
+       // ¿Es una ruta absoluta? ---> absoluteRoute verifica ello y si la ruta no es absoluta, pues la convierte a una ruta absoluta
+      const absoluteRoute = fn.verifyAbsoluteRoute(route);
+      const arrLinks = [];
+         // ¿Es un directorio? Leer el contenido del directorio
+      const checkDirectory = fn.verifyDirectoryOrFile(absoluteRoute);
+         if(checkDirectory){
+           arrLinks = checkDirectory(absoluteRoute);
+        } else {
+          // ¿Es un archivo md.
+          const checkExtesionMd = fn.verifyExtensionMd(absoluteRoute);
+            if(checkExtesionMd){
+              arrLinks = readFileWithExtensionMd(absoluteRoute);
+            } else {
+              reject('No es un archivo .md');
+            }
+        }
+         // ¿El archivo tiene links?
+        if(arrLinks === ''){
+          reject('Este archivo no contiene links');
+        } else {
+              // Array del arreglo con los links
+              // Options (validate)
+              // Validate Sí: Validar cada links por medio de peticiones HTTP ->> href, text, file, status, Ok
+              // Validate No: retorna href, text, file 
+              if(options.validate == true){
+                checkLinks(arrLinks)
+                  .then((response) => {
+                    resolve(response);
+                  });
+              } else {
+                resolve(arrLinks);
+              }
+        }
+    }   
 
-    // ¿Es una ruta absoluta?
-  
-    // ¿Es un directorio? Leer el contenido del directorio
-    // ¿Es un archivo md.
-    // ¿El archivo tiene links?
-    // Array del arreglo con los links
-    // Options (validate)
-    // Validate Sí: Validar cada links por medio de peticiones HTTP ->> href, text, file, status, Ok
-    // Validate No: retorna href, text, file 
-     checkLinks([
-      {
-        href: 'https://nodejs.org/es/',
-        text: 'Node',
-        file: 'D:/Lab/LIM018-md-links/tools/tool.md'
-      },
-      {
-        href: 'https://es.wikipedia.org/wiki/Markdown',
-        text: 'Markdown',
-        file: 'D:/Lab/LIM018-md-links/tools/tool.md'
-      }
-    ]).then((res) => resolve(res))
   }) 
 }
 
