624e1a96f5bd8277cfbb432525fcba33b1f2f2c7
jackie2806 <jaishackeline@gmail.com>
2022-10-25 09:33:24 -0500

    implementación de CLI

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env nodo
+const objFnAxuliary = require("../src/small-functions.js");
+const objFnPrincipal = require('../src/index.js');
+
+const [,, ...args] = process.argv
+console.log(`Hello World ${args}`)
+
